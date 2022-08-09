9e4a5d2daef3d17863290d3611513818dd6a3962
Kafuu Coori <1779207228@qq.com>
2022-08-09 11:52:45 +0800

    定义Tank方向

--- src/top/kafuucoori/tank/TankFrame.java
+++ src/top/kafuucoori/tank/TankFrame.java
@@ -15,6 +15,10 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
     // 定义坐标值
     int x = 200, y = 200;
+    // 设置坦克初始方向
+    Dir dir = Dir.UP;
+    // 设置坦克单次移动的距离，私有静态，不可变
+    private static final int SPEED = 10;
 
     // 创建窗口
     public TankFrame() {
@@ -44,19 +48,66 @@ public class TankFrame extends Frame {
     public void paint(Graphics g) {
         // 填充一个矩形
         g.fillRect(x, y, 50, 50);
+
+        switch(dir) {
+            case LEFT: x -= SPEED; break;
+            case Right: x += SPEED; break;
+            case UP: y -= SPEED; break;
+            case DOWN: y += SPEED; break;
+        }
     }
 
     // 键盘监听处理
     class MyKeyListener extends KeyAdapter {
+        // 定义各按键状态
+        boolean bl = false;
+        boolean br = false;
+        boolean bu = false;
+        boolean bd = false;
+
         @Override
         public void keyPressed(KeyEvent e) {
-            x += 30;
-            repaint();
+            // 获取按键返回值
+            int key = e.getKeyCode();
+
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bl = true; break;
+                case KeyEvent.VK_RIGHT:
+                    br = true; break;
+                case KeyEvent.VK_UP:
+                    bu = true; break;
+                case KeyEvent.VK_DOWN:
+                    bd = true; break;
+                default: break;
+            }
+            // 根据按键设置坦克朝向
+            setMainTankDir();
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            super.keyReleased(e);
+            int key = e.getKeyCode();
+
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bl = false; break;
+                case KeyEvent.VK_RIGHT:
+                    br = false; break;
+                case KeyEvent.VK_UP:
+                    bu = false; break;
+                case KeyEvent.VK_DOWN:
+                    bd = false; break;
+                default: break;
+            }
+            setMainTankDir();
+        }
+
+        private void setMainTankDir() {
+            if(bl) dir = Dir.LEFT;
+            if(br) dir = Dir.Right;
+            if(bu) dir = Dir.UP;
+            if(bd) dir = Dir.DOWN;
         }
     }
 }
